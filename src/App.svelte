42fbef97e41421f1918648d7e5d11fd668aa6cb4
Benjamin Keating <ben@spreadsheetsusa.com>
2021-10-20 13:02:10 -0500

    adds tailwindcss

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,11 @@
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<style>
+<style global>
+	@tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
 	main {
 		text-align: center;
 		padding: 1em;
