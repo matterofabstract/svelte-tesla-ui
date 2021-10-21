7ead3d59f022600191626b6855eb8426f7edd3fb
Benjamin Keating <ben@spreadsheetsusa.com>
2021-10-21 01:00:56 -0500

    placeholder

--- src/screens/Home.svelte
+++ src/screens/Home.svelte
@@ -1,10 +1,14 @@
 <script>
   import { onMount } from 'svelte';
+  import { fly, fade } from 'svelte/transition';
 
   import MenuBar from '../components/MenuBar.svelte';
   import CarHudView from '../components/CarHudView.svelte';
 
+  let loaded = false;
+
   onMount(() => {
+    loaded = true;
     document.body.style.backgroundColor = '#eee';
   });
 </script>
@@ -15,7 +19,9 @@
       <CarHudView />
     </div>
     <div class="w-2/3">
-      [map shit goes here]
+      {#if loaded}
+        <img src="/assets/placeholder.jpg" alt="" class="w-auto h-full" in:fly={{ x: 400, duration: 1200, delay: 1000 }} out:fly={{ y: -10, duration: 300 }} />
+      {/if}
     </div>
   </div>
   <div class="flex-grow-0" style="height: 100px;">
