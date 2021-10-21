e39cc0813f2593482b6df6eb3da1d3273111a121
Benjamin Keating <ben@spreadsheetsusa.com>
2021-10-21 01:06:39 -0500

    no placeholder, broken icons still?

--- src/screens/Home.svelte
+++ src/screens/Home.svelte
@@ -1,14 +1,14 @@
 <script>
   import { onMount } from 'svelte';
-  import { fly, fade } from 'svelte/transition';
+  // import { fly, fade } from 'svelte/transition';
 
   import MenuBar from '../components/MenuBar.svelte';
   import CarHudView from '../components/CarHudView.svelte';
 
-  let loaded = false;
+  // let loaded = false;
 
   onMount(() => {
-    loaded = true;
+    // loaded = true;
     document.body.style.backgroundColor = '#eee';
   });
 </script>
@@ -19,9 +19,7 @@
       <CarHudView />
     </div>
     <div class="w-2/3">
-      {#if loaded}
-        <img src="/assets/placeholder.jpg" alt="" class="w-auto h-full" in:fly={{ x: 400, duration: 1200, delay: 1000 }} out:fly={{ y: -10, duration: 300 }} />
-      {/if}
+
     </div>
   </div>
   <div class="flex-grow-0" style="height: 100px;">
